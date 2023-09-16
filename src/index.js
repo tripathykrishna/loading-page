37654296385d92eb5b6a62b6899774066c07b83d
tripathykrishna <tripathyswoyamprava@gmail.com>
2023-09-16 10:33:37 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import logo from './logo2.png.png';
+import {Canvas} from '@react-three/fiber';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <header>
+     <img src={logo} alt='alt here'/> 
+    </header>
+    <Canvas>
     <App />
+    </Canvas>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
